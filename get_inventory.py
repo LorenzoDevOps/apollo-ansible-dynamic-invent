134eebce9f146cd23ad3c019a64e7ea982b788ff
Lorenzo <lorenzo.it@outlook.com>
2018-04-30 15:38:19 +0200

    Dynamic inventory

--- get_inventory.py
+++ get_inventory.py
@@ -13,6 +13,7 @@ try:
 except ImportError:
     import simplejson as json
 
+
 class ExampleInventory(object):
 
     def __init__(self):
@@ -30,14 +31,13 @@ class ExampleInventory(object):
         else:
             self.inventory = self.empty_inventory()
 
-        print json.dumps(self.inventory);
+        print(json.dumps(self.inventory))
 
     # Example inventory for testing.
     def example_inventory(self):
         return {
             'group': {
-                'hosts': ['172.81.177.239', '51.255.211.226','51.255.211.177',
-                          '137.74.107.162','137.74.85.235','51.255.211.205' ],
+                'hosts': ['164.132.182.59'],
                 'vars': {
                     'ansible_ssh_user': 'centos',
                     'ansible_ssh_private_key_file':
@@ -47,23 +47,8 @@ class ExampleInventory(object):
             },
             '_meta': {
                 'hostvars': {
-                    '172.81.177.239': {
-                        'host_specific_var': 'apollo'
-                    },
-                    '51.255.211.226': {
-                        'host_specific_var': 'user3'
-                    },
-                    '51.255.211.177': {
-                        'host_specific_var': 'v3'
-                    },
-                    '137.74.107.162': {
-                        'host_specific_var': 'v4'
-                    },
-                    '137.74.85.235': {
-                        'host_specific_var': 'v5'
-                    },
-                    '51.255.211.205': {
-                        'host_specific_var': 'v6'
+                    '164.132.182.59': {
+                        'host_specific_var': 'centos'
                     }
                 }
             }
@@ -76,9 +61,10 @@ class ExampleInventory(object):
     # Read the command line args passed to the script.
     def read_cli_args(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--list', action = 'store_true')
-        parser.add_argument('--host', action = 'store')
+        parser.add_argument('--list', action='store_true')
+        parser.add_argument('--host', action='store')
         self.args = parser.parse_args()
+
 
 # Get the inventory.
 ExampleInventory()
